86f14de07a3df5c699d6543b440408e927c52e74
mattemutti <mattemutti@gmail.com>
2024-02-21 16:01:14 +0100

    add if/else condiction

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,21 +5,40 @@
 for(let i = 1; i < 101; i++){
 
 //	console.log(i);
-	let numerodivisibileX3 = i % 3 
-if (numerodivisibileX3 == 0) {
-	console.log(i, "Fizz");
-}
-console.log(i);
+
+// salvare il valore del resto della divisione del numero / 3 (se divisibile 0 altrimenti 1) in una variabile
+	let numerodivisibileX3 = i % 3;
+
+// salvare il valore del resto della divisione del numero / 5 (se divisibile 0 altrimenti 1) in una variabile
+	let numerodivisibileX5 = i % 5;
+
+	
+	// 	- se il resto di i/3 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 se di 5 quindi stampa FizzBuzz
+	if (numerodivisibileX3 == 0 && numerodivisibileX5 == 0) {
+		console.log("FizzBuzz");
+		
+	// 	- altrimenti se il resto di i/3 è uguale a 0 e il resto di i/5 è diverso da 0 allora il numero è un multiplo di 3 quindi stampa Fizz
+	} else if (numerodivisibileX3 == 0 && numerodivisibileX5 != 0 ) {
+		console.log("Fizz");
+
+	// 	- altrimenti se il resto di i/3 è diverso da 0 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 5 quindi stampa Buzz
+	} else if (numerodivisibileX3 != 0 && numerodivisibileX5 == 0 ) {
+		console.log("Buzz");
+
+	// - se non è questo caso stampi in console log il valore di i
+	} else{
+		console.log(i);
+	}
+
+
+	// 	- altrimenti se il resto di i/3 è uguale a 0 allora il numero è un multiplo di 5 quindi stampa buzz
+
 
 }
 
 // - ad ogni loop dobbiamo controllare se quel numero è divisibile per 3 o per 5
 
-// 	- se il resto di i/3 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 se di 5 quindi stampa fizzbuzz
-// 	- altrimenti se il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 quindi stampa fizz
-// 	- altrimenti se il resto di i/3 è uguale a 0 allora il numero è un multiplo di 5 quindi stampa buzz
 	
-// - se non è questo caso stampi in console log il valore di i
 
 
 
