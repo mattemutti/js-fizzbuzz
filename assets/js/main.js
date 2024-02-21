c13c20dd56fa1e3f3abb00017a998bc1a09f13ea
mattemutti <mattemutti@gmail.com>
2024-02-21 15:13:52 +0100

    add test risultato numeri divisibili per 3 e per 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,50 @@
 
 
 // - Creare un ciclo che stampi in console i numeri da 1 a 100 con for e mettere il consolo.log(i)
+
 // - ad ogni loop dobbiamo controllare se quel numero è divisibile per 3 o per 5
+
 // 	- se il resto di i/3 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 se di 5 quindi stampa fizzbuzz
 // 	- altrimenti se il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 quindi stampa fizz
 // 	- altrimenti se il resto di i/3 è uguale a 0 allora il numero è un multiplo di 5 quindi stampa buzz
 	
 // - se non è questo caso stampi in console log il valore di i
+
+
+
+
+
+// Prova numeri divisibili per 3
+let x = 30
+console.log(x % 3);
+x = 31
+console.log(x % 3);
+x = 15
+console.log(x % 3);
+x = 55
+console.log(x % 3);
+x = 12
+console.log(x % 3);
+x = 90
+console.log(x % 3);
+x = 120
+console.log(x % 3);
+
+console.log("divisibili per 5");
+
+
+// Prova numeri divisibili per 5
+let y = 30
+console.log(y % 5);
+y = 31
+console.log(y % 5);
+y = 15
+console.log(x % 5);
+y = 55
+console.log(y % 5);
+y = 12
+console.log(y % 5);
+y = 90
+console.log(y % 5);
+y = 120
+console.log(y % 5);
