750314bcd78f3b25ac9fe74ba555a86dc639c749
mattemutti <mattemutti@gmail.com>
2024-02-21 17:12:25 +0100

    Bonus 1 append non funzionante

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
-//console.log('funzia?');
 
 
-// - Creare un ciclo che stampi in console i numeri da 1 a 100 con for e mettere il consolo.log(i)
+
+let elementList = document.getElementById("list_number");
+let ul = document.querySelector("ul.list");
+let li = document.createElement ("li");
+
+
+// - Creare un ciclo che stampi in conso le i numeri da 1 a 100 con for e mettere il consolo.log(i)
 for(let i = 1; i < 101; i++){
 
 //	console.log(i);
@@ -16,18 +21,31 @@ for(let i = 1; i < 101; i++){
 	// 	- se il resto di i/3 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 se di 5 quindi stampa FizzBuzz
 	if (numerodivisibileX3 == 0 && numerodivisibileX5 == 0) {
 		console.log("FizzBuzz");
+		//elementList.innerHTML += `<li>FizzBuzz</li>`;
 		
 	// 	- altrimenti se il resto di i/3 è uguale a 0 e il resto di i/5 è diverso da 0 allora il numero è un multiplo di 3 quindi stampa Fizz
 	} else if (numerodivisibileX3 == 0 && numerodivisibileX5 != 0 ) {
 		console.log("Fizz");
+		elementList.innerHTML += `<li>Fizz</li>`;
 
 	// 	- altrimenti se il resto di i/3 è diverso da 0 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 5 quindi stampa Buzz
 	} else if (numerodivisibileX3 != 0 && numerodivisibileX5 == 0 ) {
 		console.log("Buzz");
+		elementList.innerHTML += `<li>Buzz</li>`;
+
 
 	// - se non è questo caso stampi in console log il valore di i
-	} else{
+	} else{ 
 		console.log(i);
+		//elementList.innerHTML += `<li>${i}</li>`;
+		
+		li.append("Elemento");
+		ul.append(li);
+		li = Number();
+		console.log(typeof li);
+		
+
+
 	}
 
 
