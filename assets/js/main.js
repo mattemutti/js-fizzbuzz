17f6bd89350b9738773bcd945a09f82aa1bda940
mattemutti <mattemutti@gmail.com>
2024-02-21 15:08:16 +0100

    add site

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//console.log('funzia?');
+
+
+// - Creare un ciclo che stampi in console i numeri da 1 a 100 con for e mettere il consolo.log(i)
+// - ad ogni loop dobbiamo controllare se quel numero è divisibile per 3 o per 5
+// 	- se il resto di i/3 e il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 se di 5 quindi stampa fizzbuzz
+// 	- altrimenti se il resto di i/5 è uguale a 0 allora il numero è un multiplo di 3 quindi stampa fizz
+// 	- altrimenti se il resto di i/3 è uguale a 0 allora il numero è un multiplo di 5 quindi stampa buzz
+	
+// - se non è questo caso stampi in console log il valore di i
